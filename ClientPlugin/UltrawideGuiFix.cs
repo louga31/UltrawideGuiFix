5ae41a8bf92f255cb1fe7006afcd3e6fd8db3f58
Quentin Ormancey <23664359+louga31@users.noreply.github.com>
2023-07-14 17:47:02 +0200

    Keybind

---
+++ ClientPlugin/UltrawideGuiFix.cs
@@ -0,0 +1,35 @@
+﻿using NLog.Fluent;
+using Sandbox;
+using Sandbox.Game;
+using Sandbox.Game.Gui;
+using Sandbox.Game.World;
+using VRage.Game.Components;
+using VRage.Input;
+
+namespace ClientPlugin
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class UltrawideGuiFix : MySessionComponentBase
+    {
+        public UltrawideGuiFix()
+        {
+            
+        }
+
+        public override void HandleInput()
+        {
+            if (!MySandboxGame.IsPaused && MySession.Static.LocalHumanPlayer != null &&
+                !MyGuiScreenGamePlay.DisableInput && IsKeybindPressed())
+            {
+                Log.Debug("Key Pressed");
+                // Open Gui ???
+            }
+        }
+
+        private bool IsKeybindPressed()
+        {
+            return MyInput.Static.IsNewGameControlPressed(MyControlsSpace.BUILD_SCREEN) &&
+                   MyInput.Static.IsAnyAltKeyPressed() && MyInput.Static.IsAnyCtrlKeyPressed();
+        }
+    }
+}
